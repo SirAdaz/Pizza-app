7725132e96bf7eb0c718293861513a08b6a238a8
Adaz SirAdaz <mrxptdrgame@gmail.com>
2024-11-07 14:44:02 +0100

    footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <footer className='bg-stone-700 text-white text-center flex p-6 justify-center items-center'>
+        <p>&copy; 2023 Pizza App - All rights reserved - 2024</p>
+    </footer>
+  )
+}
